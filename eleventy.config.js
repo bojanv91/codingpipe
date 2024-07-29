daadd0635c0fef813120c289d45440eefc00ade4
bojanv91 <1328380+bojanv91@users.noreply.github.com>
2024-07-29 18:46:40 +0200

    Update site's css

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const pluginTimeToRead = require('eleventy-plugin-time-to-read');
 
 const pluginDrafts = require("./eleventy.config.drafts.js");
 const pluginImages = require("./eleventy.config.images.js");
@@ -37,6 +38,13 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginNavigation);
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPlugin(pluginBundle);
+
+	eleventyConfig.addPlugin(pluginTimeToRead, {
+		style: 'short',
+		output: function (data) {
+			return data.timing + ' read';
+		}
+	});
 
 	// Filters
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
